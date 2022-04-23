fc3e3781436bb459e3503936a912e0d7ed79dd1b
alelubos <alelubos@gmail.com>
2022-04-23 15:51:48 +0200

    reset scripts.js & initialized pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
-alert("Hello World");
+let pokemonList = [];
 
-let favoriteFood = "Empanadas";
-document.write(favoriteFood);
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 0.7,
+  types: ["grass", "poison"],
+};
+
+pokemonList[1] = {
+  name: "Sandslash",
+  height: 1,
+  types: ["ground"],
+};
+
+pokemonList[2] = {
+  name: "Onix",
+  height: 8.8,
+  types: ["rock", "ground"],
+};
+
+pokemonList[3] = {
+  name: "Oddish",
+  height: 0.5,
+  types: ["grass", "poison"],
+};
+
+pokemonList[4] = {
+  name: "Paras",
+  height: 0.3,
+  types: ["grass", "bug"],
+};
