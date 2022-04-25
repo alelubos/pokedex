55e55c55b9c10f99e5886bf9a20027901ad6bd72
alelubos <alelubos@gmail.com>
2022-04-25 17:19:27 +0200

    added list of Pokemon
    -Modified the way of adding Pokemon objects to pokemonList using push();
    -use document.write() to output the list of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,45 @@
 let pokemonList = [];
 
-pokemonList[0] = {
-  name: "Bulbasaur",
-  height: 0.7,
-  types: ["grass", "poison"],
-};
+pokemonList.push(
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Sandslash",
+    height: 1,
+    types: ["ground"],
+  },
+  {
+    name: "Onix",
+    height: 8.8,
+    types: ["rock", "ground"],
+  },
+  {
+    name: "Oddish",
+    height: 0.5,
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Paras",
+    height: 0.3,
+    types: ["grass", "bug"],
+  }
+);
+// Write a title and opening tag for Pokemon list
+document.write("<h1>My selection of Pokemon</h1><ul>");
 
-pokemonList[1] = {
-  name: "Sandslash",
-  height: 1,
-  types: ["ground"],
-};
+// Write list of Pokemons
+for (let i = 0; i < pokemonList.length; i++) {
+  let text = `${pokemonList[i].name} (height: ${pokemonList[i].height})`;
 
-pokemonList[2] = {
-  name: "Onix",
-  height: 8.8,
-  types: ["rock", "ground"],
-};
+  // Add comment for Big pokemon
+  if (pokemonList[i].height > 5) {
+    text += " - Wow, that's big!";
+  }
+  document.write(`<li>${text}</li>`);
+}
 
-pokemonList[3] = {
-  name: "Oddish",
-  height: 0.5,
-  types: ["grass", "poison"],
-};
-
-pokemonList[4] = {
-  name: "Paras",
-  height: 0.3,
-  types: ["grass", "bug"],
-};
+// Write the closing tag of the list
+document.write("</ul>");
