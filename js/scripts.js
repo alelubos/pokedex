63ed44e1b2d22b9f259691115cb0f0aa8f585261
alelubos <alelubos@gmail.com>
2022-04-27 00:47:45 +0200

    refactored script - pokemonRepository as IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,79 @@
-let pokemonList = [];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Sandslash",
+      height: 1,
+      types: ["ground"],
+    },
+    {
+      name: "Onix",
+      height: 8.8,
+      types: ["rock", "ground"],
+    },
+    {
+      name: "Oddish",
+      height: 0.5,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Paras",
+      height: 0.3,
+      types: ["grass", "bug"],
+    },
+  ];
 
-pokemonList.push(
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Sandslash",
-    height: 1,
-    types: ["ground"],
-  },
-  {
-    name: "Onix",
-    height: 8.8,
-    types: ["rock", "ground"],
-  },
-  {
-    name: "Oddish",
-    height: 0.5,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Paras",
-    height: 0.3,
-    types: ["grass", "bug"],
+  function getAll() {
+    return pokemonList;
   }
-);
+
+  function add(pokemon) {
+    if (pokemon.name && pokemon.height && pokemon.types) {
+      pokemonList.push(pokemon);
+    } else {
+      alert("Invalid Pokemon! won't be added to the list... Please try again!");
+    }
+  }
+
+  function filter(name) {
+    let nameLowCase = name.toLowerCase();
+    let result = pokemonList.filter(
+      (pokemon) => pokemon.name.toLowerCase() === nameLowCase
+    )[0];
+    return result || "Pokemon not found!";
+  }
+
+  return {
+    getAll,
+    add,
+    filter,
+  };
+})();
+
+pokemonRepository.add({
+  name: "Pikachu",
+  height: 0.4,
+  types: ["ground"],
+});
+
+console.log(pokemonRepository.filter("Pikachu"));
+
 // Write a title and opening tag for Pokemon list
 document.write("<h1>My selection of Pokemon</h1><ul>");
 
-// Write list of Pokemons
-for (let i = 0; i < pokemonList.length; i++) {
-  let text = `<b>${pokemonList[i].name}</b> (height: ${pokemonList[i].height})`;
+// Create list of Pokemons
+pokemonRepository.getAll().forEach((pokemon) => {
+  // create item text
+  let itemText = `<b>${pokemon.name}</b> (height: ${pokemon.height})`;
+  // add special comment for Big Pokemon
+  if (pokemon.height > 5) itemText += "<span> - Wow, that's big!</span>";
+  // Write item text to document
+  document.write(`<li>${itemText}</li>`);
+});
 
-  // Add comment for Big pokemon
-  if (pokemonList[i].height > 5) {
-    text += "<span> - Wow, that's big!</span>";
-  }
-  document.write(`<li>${text}</li>`);
-}
-
-// Write the closing tag of the list
+// Write the list closing tag
 document.write("</ul>");
